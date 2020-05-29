e9fc8b26a5ad82b0e9c3992a2435d13943ac10d6
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2020-05-30 01:37:23 +0530

    Login Module Added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const db = require("../database/config");
+const jwt = require("jsonwebtoken");
 
 exports.postSignup = async (req, res) => {
   try {
@@ -13,10 +14,12 @@ exports.postSignup = async (req, res) => {
     }
     const hashedPassword = await bcrypt.hash(password, 12);
     const result = await db.execute(
-      "INSERT INTO users (username, password) VALUES (?, ?)",
+      `
+      INSERT INTO users (username, password)
+      VALUES (?, ?)
+      `,
       [username, hashedPassword]
     );
-    console.log(result);
     res.status(200).json({
       status: "OK",
     });
@@ -31,6 +34,60 @@ exports.postSignup = async (req, res) => {
       res.status(500).json({
         status: "ERROR",
         error: "Internal Server Error.",
+      });
+    }
+  }
+};
+
+exports.postLogin = async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+      return res.status(400).json({
+        status: "ERROR",
+        error: "Bad Request. Invalid Parameters.",
+      });
+    }
+    const result = await db.execute(
+      `
+    SELECT * 
+    FROM users
+    WHERE username = ?
+    `,
+      [username]
+    );
+    if (!result[0].length) {
+      throw new Error("403:invalidAuth");
+    }
+    const user = result[0][0];
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      throw new Error("403:invalidAuth");
+    }
+    const authToken = await jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET_KEY,
+      {
+        issuer: "gitaalekhyapaul",
+        expiresIn: "1h",
+      }
+    );
+    res.status(200).json({
+      status: "OK",
+      authToken: authToken,
+    });
+  } catch (err) {
+    if (err.message === "403:invalidAuth") {
+      res.status(403).json({
+        status: "ERROR",
+        error: "Username or Password Wrong.",
+      });
+    } else {
+      console.log(err);
+      res.status(500).json({
+        status: "ERROR",
+        error: "Internal Server Error",
       });
     }
   }
--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const authController = require("../controllers/auth");
 
 router.post("/signup", authController.postSignup);
+router.post("/login", authController.postLogin);
 
 module.exports = router;
